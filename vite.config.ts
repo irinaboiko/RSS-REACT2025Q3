552344be3e5d89a8bf4b525e18d9b371931e5799
irinaboiko <irinaboiko95@gmail.com>
2025-08-20 16:06:33 +0300

    init: install dependencies, remove unnecessary files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
 });
